b1b2be21e93c6fea4ef65e48057b6b65da999971
Rhys Halsey-Watkins <rhyshalsey@gmail.com>
2021-11-20 19:54:01 -0500

    Added Next.js to project and created folders for client and blockchain

---
+++ client/pages/index.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { ethers } from "ethers";
+
+export default function App() {
+  const wave = () => {};
+
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">👋 Hey there!</div>
+
+        <div className="bio">
+          I am farza and I worked on self-driving cars so that&apos;s pretty
+          cool right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
