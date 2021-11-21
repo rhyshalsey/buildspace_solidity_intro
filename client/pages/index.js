c06698d3b850015c64effb8f9985ff68a526c1a1
Rhys Halsey-Watkins <rhyshalsey@gmail.com>
2021-11-20 20:39:18 -0500

    Updated client home description

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,22 +1,33 @@
 import * as React from "react";
+import Head from "next/head";
 import { ethers } from "ethers";
 
 export default function App() {
   const wave = () => {};
 
   return (
-    <div className="mainContainer">
-      <div className="dataContainer card">
-        <div className="header">👋 Hey there!</div>
+    <>
+      <Head>
+        <title>What is your favorite animal on the blockchain? </title>
+      </Head>
+      <div className="mainContainer">
+        <div className="dataContainer card">
+          <div className="header">👋 Hey there!</div>
 
-        <div className="bio">
-          Connect your Ethereum wallet tell me about your favorite animal!
+          <div className="bio">
+            Connect your Ethereum wallet tell me about your favorite animal!
+          </div>
+
+          <div className="bio">
+            This simple app allows you to connect your MetaMask wallet and enter
+            your favorite animal. It will then be saved to the blockchain!
+          </div>
+
+          <button className="waveButton" onClick={wave}>
+            What is your favorite animal?
+          </button>
         </div>
-
-        <button className="waveButton" onClick={wave}>
-          What is your favorite animal?
-        </button>
       </div>
-    </div>
+    </>
   );
 }
